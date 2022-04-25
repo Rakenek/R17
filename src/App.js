d4ff68f1485c79dff9b124147b307a2245703352
Rakenek <kamilotawski@gmail.com>
2022-04-25 14:57:09 +0200

    RS11v1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
+import AvailableMeals from "./components/Meals/AvailableMeals";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <MealsSummary />
+        <AvailableMeals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals"
+import {Fragment} from react;
+
+const Meals=()=>{
+    <Fragment>
+        <MealsSummary/>
+        <AvailableMeals/>
+    </Fragment>
+}
